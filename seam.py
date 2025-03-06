3df8670721344f82151b3976746124967cf8dc8e
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-03-05 19:17:25 -0500

    Update seam.py

--- seam.py
+++ seam.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template,request
-
+app = Flask(__name__)
+from flask import Flask, render_template,request
+import pickle
 # Encode input text (prompt)
 
 from tensorflow.keras.layers import TextVectorization
@@ -7,9 +9,6 @@ with open('emotions.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
 X_test = ["hello"]
 vectorizer = TextVectorization(max_tokens=50,output_mode='int')
-
-app = Flask(__name__)
-
 @app.route("/", methods=["GET", "POST"])  # Both GET and POST are allowed here
 def home():
     user_input = None
@@ -19,8 +18,8 @@ def home():
         user_input = request.form.get("user_input")
         print(f"User input: {user_input}")
         X_test = user_input
-        vectorizer.adapt(X_test)
-        vectorized_texts = vectorizer(X_test)
+        vectorizer.adapt(user_input)
+        vectorized_texts = vectorizer(user_input)
         
         predictions = loaded_model.predict(vectorized_texts)
         print(predictions)
@@ -28,17 +27,6 @@ def home():
         bin = (predictions >= threshold).astype(int)
         labels_text = ['positive' if label == 1 else 'negative' for label in bin.flatten()]
         print(labels_text)
-
-        
-
-    # Generate text from the model
-        
-
-    # Decode and print the output
-        
-        # For debugging: print user input to terminal
-        return jsonify({"generated_text": labels_text})
-    return render_template("index.html",labels_text=labels_text)
 
 if __name__ == "__main__":
     app.run(debug=True)
